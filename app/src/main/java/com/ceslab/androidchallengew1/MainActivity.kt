4e72411e3bd0c50b71a2707f967d52730518e40a
Viet Hua <huaviet999@gmail.com>
2021-03-28 13:43:24 +0700

    switch screen

--- app/src/main/java/com/ceslab/androidchallengew1/MainActivity.kt
+++ app/src/main/java/com/ceslab/androidchallengew1/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.ceslab.androidchallengew1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_verification)
+        setContentView(R.layout.activity_welcome)
+        val btnStart = findViewById<MaterialButton>(R.id.btn_start)
+        btnStart.setOnClickListener {
+            val intent = Intent(this,BoardingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
