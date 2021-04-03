1ec055abf6d03437a3a1e22ce25c4f86caec1c50
ndv260599 <nguyenducvu26051999@gmail.com>
2021-04-03 16:10:37 +0700

    no message

--- app/src/main/java/com/ceslab/androidchallengew1/SignInActivity.kt
+++ app/src/main/java/com/ceslab/androidchallengew1/SignInActivity.kt
@@ -3,19 +3,31 @@ package com.ceslab.androidchallengew1
 import android.content.Intent
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.ceslab.androidchallengew1.model.User
 import com.google.android.material.button.MaterialButton
 
-class SignInActivity : AppCompatActivity(){
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_signin)
-
+        val user: User = User()
+        var edEmail = findViewById<EditText>(R.id.ed_email)
+        var edPassword = findViewById<EditText>(R.id.ed_password)
         val btnSignin = findViewById<MaterialButton>(R.id.btn_sign_in)
         btnSignin.setOnClickListener {
-            val intent = Intent(this,VerificationActivity::class.java)
-            startActivity(intent)
+            if (edEmail.text.toString().trim().equals(user.email) && edPassword.text.toString().trim().equals(user.password)) {
+                val intent = Intent(this, ProfileActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this@SignInActivity, "Sign in fail", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
